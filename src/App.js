ab6378cd6a54e50b29f038ead984fedf902fd10b
kristinaordenans <okristin92@gmail.com>
2023-09-13 08:28:42 +0300

    start

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header"> 
-        <h1>HALLO!</h1>
+        <h1>HALLO you!</h1>
       </header>
     </div>
   );

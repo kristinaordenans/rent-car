871f008158dfc631902c0ab9990bfe1c53674e5a
kristinaordenans <okristin92@gmail.com>
2023-09-15 20:28:20 +0300

    styled home page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,17 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import css from '../Header/Header.module.css'
 
 const Header =() => {
     return (
-        <nav>
-            <Link to="/">Home</Link>
-            <Link to="/catalog">Catalog</Link>
-            <Link to="/favorites">Favorites</Link>
-        </nav>
+        <div className={css.container}>  
+            <nav className={css.navContainer}>
+            <Link to="/"className={css.navTitle}>Rental Service</Link>
+            <Link to="/" className={css.navLink}>Home</Link>
+            <Link to="/catalog" className={css.navLink}>Catalog</Link>
+            <Link to="/favorites" className={css.navLink}>Favorites</Link>
+          </nav>
+            <Link href="tel:+380730000000" className={css.navLinkContact}>+38(073)000-00-00</Link>
+        </div>
     )
 }
-export default Header
+export default Header;
